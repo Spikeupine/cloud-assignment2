504a8abd664920f114d98daec2e5aece02ca8461
Solveig <solvlang@stud.ntnu.no>
2024-04-04 13:04:30 +0200

    Updated constants

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-9
-	consts.go,d\b\dbda4204c1535f683d2144518b8ebb899b681f55
 B
 internal/consts.go,9\c\9c2e0be23c78840c42dcb1362e4aacffdd051a74
 S
--- internal/consts.go
+++ internal/consts.go
@@ -2,29 +2,29 @@ package internal
 
 // API's
 // Link to the countries api
-const countriesApi = "http://129.241.150.113:8080/v3.1/"
+const CountriesApi = "http://129.241.150.113:8080/v3.1/"
 
 // Link to the currency api
-const currencyApi = "http://129.241.150.113:9090/currency/"
+const CurrencyApi = "http://129.241.150.113:9090/currency/"
 
 // The different paths that can be used by user.
 // Path to dashboards
-const dashboardsPath = "/dashboard/v1/dashboards/"
+const DashboardsPath = "/dashboard/v1/dashboards/"
 
 // Path to notifications
-const notificationsPath = "/dashboard/v1/notifications/"
+const NotificationsPath = "/dashboard/v1/notifications/"
 
 // Path to registrations
-const registrationsPath = "/dashboard/v1/registrations/"
+const RegistrationsPath = "/dashboard/v1/registrations/"
 
 // Path to status
-const statusPath = "/dashboard/v1/status/"
+const StatusPath = "/dashboard/v1/status/"
 
 // The type of the content. How to present it or read it.
-const contentTypeJson = "application/json"
+const ContentTypeJson = "application/json"
 
 // const of string value Content type.
-const applicationJson = "Content type: "
+const ApplicationJson = "Content type: "
 
 // Example of ISO code to be used to check if services that require ISO code is available.
-const isoExample = "no"
+const IsoExample = "alpha?codes=no"
