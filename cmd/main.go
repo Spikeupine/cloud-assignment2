fd6066fcf59a97db9898236fbd8c6446849bc265
Trygve Sollund <trygvsol@stud.ntnu.no>
2024-04-09 16:22:04 +0200

    moved firebase functionality to seperate function

--- cmd/main.go
+++ cmd/main.go
@@ -24,7 +24,29 @@ var ct = 0
 
 // This is the start point of the entire service.
 func main() {
+	firebaseConnect()
+	//Gets the port from the environment. If empty, sets it to 8080 as default.
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("$PORT not set. Default: 8080")
+		port = "8080"
+	}
+	addr := ":" + port
 
+	// Starts the server
+	log.Println("Starting server on port " + port + " ...")
+	http.HandleFunc(internal.DashboardsPath, handlers.HandleRestcountriesapi)
+	http.HandleFunc(internal.RegistrationsPath, func(w http.ResponseWriter, r *http.Request) {
+		handlers.RegistrationsHandlerPost(client, w, r)
+	})
+	log.Printf("Firestore REST service listening on %s ...\n", addr)
+	if errServ := http.ListenAndServe(addr, nil); errServ != nil {
+		panic(errServ)
+	}
+
+}
+
+func firebaseConnect() {
 	// Firebase initialisation
 	ctx = context.Background()
 
@@ -55,24 +77,4 @@ func main() {
 			log.Fatal("Closing of the Firebase client failed. Error:", errClose)
 		}
 	}()
-
-	//Gets the port from the environment. If empty, sets it to 8080 as default.
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Println("$PORT not set. Default: 8080")
-		port = "8080"
-	}
-	addr := ":" + port
-
-	// Starts the server
-	log.Println("Starting server on port " + port + " ...")
-	http.HandleFunc(internal.DashboardsPath, handlers.HandleRestcountriesapi)
-	http.HandleFunc(internal.RegistrationsPath, func(w http.ResponseWriter, r *http.Request) {
-		handlers.RegistrationsHandlerPost(client, w, r)
-	})
-	log.Printf("Firestore REST service listening on %s ...\n", addr)
-	if errServ := http.ListenAndServe(addr, nil); errServ != nil {
-		panic(errServ)
-	}
-
 }
