f07d8b77815e16112c8cb46257b0c946df638050
Sofia Serine <sofiasm@stud.ntnu.no>
2024-04-09 16:44:58 +0200

    made populated dashboard struct

--- internal/structs.go
+++ internal/structs.go
@@ -1,6 +1,6 @@
 package internal
 
-//Struct for holding information about country from restcountries API.
+// Struct for holding information about country from restcountries API.
 type RestCountriesStruct struct {
 	Name        string                  `json:"common"`
 	Iso         string                  `json:"cca2"`
@@ -14,4 +14,23 @@ type RestCountriesStruct struct {
 type CurrencyInfo struct {
 	Name   string `json:"name"`
 	Symbol string `json:"symbol"`
+}
+
+// Request for an individual dashboard identified by its ID (same as the corresponding configuration ID)
+type PopulatedDashboard struct {
+	Country  string `json:"country"`
+	IsoCode  string `json:"isoCode"`
+	Features struct {
+		Temperature   float64 `json:"temperature"`
+		Precipitation float64 `json:"precipitation"`
+		Capital       string  `json:"capital"`
+		Coordinates   struct {
+			Latitude  float32 `json:"latitude"`
+			Longitude float32 `json:"longitude"`
+		} `json:"coordinates"`
+		Population       int                `json:"population"`
+		Area             float64            `json:"area"`
+		TargetCurrencies map[string]float64 `json:"targetCurrencies"`
+	} `json:"features"`
+	LastRetrieval string `json:"lastRetrieval"`
 }
