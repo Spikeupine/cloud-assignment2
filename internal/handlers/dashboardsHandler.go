5fcb6bfdc81a68b6a22299e9c9569d83da50d25f
Sofia Serine <sofiasm@stud.ntnu.no>
2024-04-09 21:52:29 +0200

    created a getDashboard function, needs to be fixed

--- internal/handlers/dashboardsHandler.go
+++ internal/handlers/dashboardsHandler.go
@@ -1,5 +1,30 @@
 package handlers
 
+import (
+	"assignment-2/internal"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 func DashboardsHandler() {
-	
+
+}
+
+func getDashboard(id string, client *http.Client) (internal.PopulatedDashboard, error) {
+	url := internal.DashboardsPath + id
+	response, err := client.Get(url)
+
+	if err != nil {
+		return internal.PopulatedDashboard{}, fmt.Errorf("Failed to fetch information: %v", err)
+	}
+	defer response.Body.Close()
+
+	var dashboard internal.PopulatedDashboard
+	err = json.NewDecoder(response.Body).Decode(&dashboard)
+	if err != nil {
+		return internal.PopulatedDashboard{}, fmt.Errorf("Failed to decode information: %v", err)
+	}
+
+	return dashboard, nil
 }
