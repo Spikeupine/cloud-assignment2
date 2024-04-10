8c32cea574d5c5dbcff2cc5899243ef16131b4aa
Sofia Serine <sofiasm@stud.ntnu.no>
2024-04-10 12:40:39 +0200

    refactored dashboardhandler

--- internal/handlers/dashboardsHandler.go
+++ internal/handlers/dashboardsHandler.go
@@ -2,54 +2,44 @@ package handlers
 
 import (
 	"assignment-2/internal"
-	"cloud.google.com/go/firestore"
 	"encoding/json"
 	"net/http"
 )
 
 // DashboardsHandler handles requests related to dashboards
-func DashboardsHandler(basics internal.Basics, client *firestore.Client) {
-	switch basics.Request.Method {
+func DashboardsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
 	case http.MethodGet:
-		// Retrieve the ID
-		id := basics.ID
-		// Check if the ID is empty
+		id := r.URL.Query().Get("id")
 		if len(id) < 1 {
-			http.Error(basics.ResponseWriter, "invalid ID", http.StatusBadRequest)
+			http.Error(w, "invalid ID", http.StatusBadRequest)
 			return
 		}
 
-		// Retrieve the dashboard
-		dashboard, err := getDashboard(id, client)
+		dashboard, err := getDashboard(id)
 		if err != nil {
-			http.Error(basics.ResponseWriter, "error retrieving dashboard: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "error retrieving dashboard: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// Encode the dashboard data as JSON
-		response, err := json.Marshal(dashboard)
+		resp, err := json.Marshal(dashboard)
 		if err != nil {
-			http.Error(basics.ResponseWriter, "error encoding response: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "error encoding response: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		// Set the content type and write the response
-		basics.ResponseWriter.Header().Set("Content-Type", "application/json")
-		basics.ResponseWriter.WriteHeader(http.StatusOK)
-		basics.ResponseWriter.Write(response)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(resp)
 	default:
-		http.Error(basics.ResponseWriter, "method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 	}
-
 }
 
-// getDashboard retrieves dashboard information
-func getDashboard(id string, client *firestore.Client) (internal.PopulatedDashboard, error) {
-	// Create an empty dashboard.
+// getDashboard retrieves dashboard information.
+func getDashboard(id string) (internal.PopulatedDashboard, error) {
 	var dashboard internal.PopulatedDashboard
 
-	// Ignore binder and client for now
-	// Currently, not using the ID and the client
+	// Ignore binder and client for now.
 
 	return dashboard, nil
 }
