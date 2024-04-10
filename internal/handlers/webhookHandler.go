dba451c65b60fb98418e5236da16a38d8737e55a
Solveig <solvlang@stud.ntnu.no>
2024-04-10 15:05:28 +0200

    Made WebhookHandler switchcase for http-method

--- internal/handlers/webhookHandler.go
+++ internal/handlers/webhookHandler.go
@@ -2,38 +2,29 @@ package handlers
 
 import (
 	"assignment-2/internal"
-	"encoding/json"
 	"net/http"
 )
 
+var collectionName string = "dashboards"
+
 var webhooks = []internal.Webhook{}
 
-//Manages registration of the different types of webhooks.
+// Manages registration of the different types of webhooks.
 func WebhookHandler(w http.ResponseWriter, r *http.Request) {
 
 	//Handles POST and GET
 	switch r.Method {
 	case http.MethodPost:
-
-		//Initializes empty struct of webhook to populate
-		webhook := internal.Webhook{}
-
-		//Populates the webhook struct with content from body of response.
-		err := json.NewDecoder(r.Body).Decode(&webhook)
-		if err != nil {
-			http.Error(w, "Error in registration of webhook", http.StatusInternalServerError)
-		}
-		webhooks = append(webhooks, webhook)
+		RegisterWebhook(w, r, collectionName)
 		break
-
 	case http.MethodGet:
-		//Returns all webhooks for now
-		//todo: Handle the get for specific webhooks
-		err := json.NewEncoder(w).Encode(webhooks)
-		if err != nil {
-			http.Error(w, "Something went wrong when getting all webhooks "+err.Error(), http.StatusServiceUnavailable)
-		}
+		GetWebhook(w, r, collectionName)
 		break
-
+	case http.MethodDelete:
+		DeleteWebhook(w, r, collectionName)
+		break
+	default:
+		http.Error(w, "Method '"+r.Method+"' not supported. Currently method '"+http.MethodPost+
+			"', '"+http.MethodDelete+"' and '"+http.MethodGet+"' is supported.", http.StatusNotImplemented)
 	}
 }
