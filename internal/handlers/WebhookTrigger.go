95d800d37a60d05410fe29b193d4505ed4bac628
Solveig <solvlang@stud.ntnu.no>
2024-04-15 00:27:32 +0200

    Commented on webhookTrigger

--- internal/handlers/WebhookTrigger.go
+++ internal/handlers/WebhookTrigger.go
@@ -12,7 +12,7 @@ import (
 // IncrementCallCount increments all webhooks that is subscribing to the countryCode
 func IncrementCallCount(w http.ResponseWriter, countryCode string) {
 
-	//Gets all webhooks stored in webhooks, a list of webhook, or error.
+	//Method that returns a list of all the webhooks registered to specified collection, and error if any.
 	webhooks, err := database.GetAllWebhooks(w, collectionNameWebhooks)
 	if err != nil {
 		http.Error(w, "Error when getting a list of all the webhooks :"+err.Error(), http.StatusInternalServerError)
@@ -20,9 +20,13 @@ func IncrementCallCount(w http.ResponseWriter, countryCode string) {
 
 	// Goes through all the webhooks in the list.
 	for _, webhook := range webhooks {
-		// increments the count of calls on each of that specified country code
+
+		// increments the count of calls on each of that specified country code, so every time a country with that
+		// iso code is registered to registrations endpoint, it adds one to the count.
 		if countryCode == webhook.Country || countryCode == "" {
 			webhook.Calls++
+
+			//If-check to find the first time the webhook is registered. Takes it to invocation-method below.
 			if webhook.Calls == 1 {
 				invokeWebhook(webhook.Url, internal.Webhook{
 					WebhookId: webhook.WebhookId,
@@ -30,6 +34,8 @@ func IncrementCallCount(w http.ResponseWriter, countryCode string) {
 					Calls:     webhook.Calls,
 				})
 			}
+
+			//Uses method to update the call count on the webhook in question.
 			err := database.UpdateTheCallCount(collectionNameWebhooks, webhook.WebhookId, webhook.Calls)
 			if err != nil {
 				http.Error(w, "Error when updating call count in webhook trigger :"+err.Error(), http.StatusNotFound)
@@ -39,7 +45,7 @@ func IncrementCallCount(w http.ResponseWriter, countryCode string) {
 	}
 }
 
-// invokeWebhook invokes a POST request to the webhook at url with the body data
+// invokeWebhook invokes a POST request to the webhook at url specified in registered webhook with the body data
 func invokeWebhook(url string, data internal.Webhook) {
 
 	payload, err := json.Marshal(data)
@@ -48,6 +54,7 @@ func invokeWebhook(url string, data internal.Webhook) {
 		return
 	}
 
+	//Posts the content as content type json to the url. In our instance, a webhook.site page.
 	resp, err := http.Post(url, internal.ContentTypeJson, bytes.NewBuffer(payload))
 	if err != nil {
 		log.Printf(err.Error())
