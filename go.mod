2a2985303594523185cac7e4fc30e18e5948565b
Solveig <solvlang@stud.ntnu.no>
2024-04-18 14:39:28 +0200

    testing making of tests

--- go.mod
+++ go.mod
@@ -3,28 +3,32 @@ module assignment-2
 go 1.22
 
 require (
+	cloud.google.com/go/firestore v1.15.0
 	firebase.google.com/go v3.13.0+incompatible
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.8.4
 	google.golang.org/api v0.172.0
+	google.golang.org/grpc v1.62.1
 )
 
 require (
 	cloud.google.com/go v0.112.1 // indirect
 	cloud.google.com/go/compute v1.24.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/firestore v1.15.0 // indirect
 	cloud.google.com/go/iam v1.1.7 // indirect
 	cloud.google.com/go/longrunning v0.5.5 // indirect
 	cloud.google.com/go/storage v1.40.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.3 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
@@ -42,6 +46,6 @@ require (
 	google.golang.org/genproto v0.0.0-20240213162025-012b6fc9bca9 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240314234333-6e1732d8331c // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
-	google.golang.org/grpc v1.62.1 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/handlers/notificationsHandler_test.go
+++ internal/handlers/notificationsHandler_test.go
@@ -1,19 +1,103 @@
 package handlers
 
 import (
-	"assignment-2/database"
-	"os"
+	"assignment-2/internal"
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
 const TestCollection = "testWebhooks"
 
-func TestMain(m *testing.M) {
-	database.FirebaseConnect()
-	code := m.Run()
-	err := database.FireBaseCloseConnection
-	if err != nil {
-		os.Exit(1)
+func TestDeleteWebhook(t *testing.T) {
+
+	hook := internal.Webhook{
+		Url:     "https://localhost:8080/dashboards/v1/notifications/",
+		Country: "NO",
+		Event:   "REGISTER",
 	}
-	os.Exit(code)
+
+	// marshal a webhook registration
+	body, err := json.Marshal(hook)
+
+	// register the webhook
+	// create a request
+	req := httptest.NewRequest(http.MethodPost, internal.NotificationsPath, bytes.NewBuffer(body))
+	req.Header.Add("content-type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	// call the handler function
+	WebhookRegistration(recorder, req, collectionNameWebhooks)
+
+	req.Header.Add(internal.ApplicationJson, internal.ContentTypeJson)
+
+	// create test recorder
+	rec := httptest.NewRecorder()
+	// calls the handler function
+	DeleteWebhook(rec, req, TestCollection, hook.WebhookId)
+
+	// Create a ResponseRecorder to record the response
+	recorder = httptest.NewRecorder()
+	// Check the response body
+	expectedResponseBody := http.StatusBadRequest
+
+	intToUse, err := strconv.Atoi(recorder.Body.String())
+	if err != nil {
+		log.Fatal("Error error pants on fire")
+	}
+
+	if intToUse != expectedResponseBody {
+		t.Errorf("handler returned unexpected body: got %v, want %v", recorder.Body.String(), expectedResponseBody)
+	}
+
+	/*
+			// calls the handler function
+			WebhookRegistration(rec, request, TestCollection)
+
+			var hook internal.Webhook
+
+			err = json.Unmarshal(rec.Body.Bytes(), &hook)
+			if err != nil {
+				t.Fatal("Error unmarshalling response : " + err.Error())
+			}
+			var tests []internal.Webhook
+
+			tests[0] = hook
+			hook.WebhookId = ""
+			tests[1] = hook
+
+			// iterate through the test cases
+			for _, testCase := range tests {
+				t.Run(testCase.WebhookId, func(t *testing.T) {
+
+					req := httptest.NewRequest(http.MethodDelete, internal.NotificationsPath+testCase.WebhookId, nil)
+					req.Header.Add(internal.ApplicationJson, internal.ContentTypeJson)
+
+					// create the recorder
+					rec := httptest.NewRecorder()
+
+					// calls the handler function
+					DeleteWebhook(rec, req, TestCollection, testCase.WebhookId)
+
+					requestDeletedWebhook := httptest.NewRequest(http.MethodGet, internal.NotificationsPath+testCase.WebhookId, nil)
+					req.Header.Add(internal.ApplicationJson, internal.ContentTypeJson)
+
+					// Asserts what is expected
+					asserts := assert.New(t)
+					asserts.Equal(requestDeletedWebhook.Response, rec.Code)
+					asserts.Equal(requestDeletedWebhook.Header, rec.Header().Get("content-type"))
+				})
+			}
+
+			// Makes sure that there are zero webhooks left
+			count, err := database.CountWebhooks(TestCollection)
+			assert.NoError(t, err)
+			assert.Equal(t, 0, count)
+		}
+	*/
 }
