332a261af54d57d4605cecfa3adb19cecdec4676
Sofia Serine <sofiasm@stud.ntnu.no>
2024-04-20 17:23:56 +0200

    added setup and teardown to status test

--- statusHandler_test.go
+++ statusHandler_test.go
@@ -1,1 +1,39 @@
 package assignment_two
+
+import (
+	"assignment-2/internal/handlers"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var (
+	testServer *httptest.Server
+	testClient http.Client
+)
+
+func statusSetup() {
+	// Setting up the test server
+	testServer = httptest.NewServer(http.HandlerFunc(handlers.StatusHandler))
+	fmt.Println("Running test server on " + testServer.URL)
+
+	// Setting up the client
+	testClient = http.Client{}
+}
+
+func statusTeardown() {
+	// Closing the test server
+	testServer.Close()
+	fmt.Println("Closing test server on " + testServer.URL)
+
+	// Closing the client
+	testClient.CloseIdleConnections()
+}
+
+func TestStatusHandler(t *testing.T) {
+	// Run setup at the start of the test and teardown at the end
+	statusSetup()
+	defer statusTeardown()
+
+}
