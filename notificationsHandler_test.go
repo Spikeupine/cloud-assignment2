6a7de8b769da04cb8c7a472a5c52ab90216c33e6
Solveig <solvlang@stud.ntnu.no>
2024-04-20 18:11:12 +0200

    Moved TestDeleteWebhook to the bottom so it deletes all the tests

--- notificationsHandler_test.go
+++ notificationsHandler_test.go
@@ -128,77 +128,6 @@ func TestWebhookRegistration(t *testing.T) {
 
 }
 
-// TestDeleteWebhook deletes the webhooks that has been added to the collection of webhook id's within this test file.
-// It ranges over this list, and deletes them by creating http DELETE requests, and passing this as parameter to
-// DeleteWebhook method of handler. The recorded http status code is then later compared, and a get request to the
-// individual webhooks is tried sent (and test is failed if it successfully manages to retrieve webhook supposed
-// to be deleted). Also empties list after all is deleted from list, and asserts it empty.
-func TestDeleteWebhook(t *testing.T) {
-
-	err := godotenv.Load()
-	if err != nil {
-		os.Exit(1)
-	}
-	database.FirebaseConnect()
-
-	//Initializes client.
-	client := http.Client{}
-
-	rec := httptest.NewRecorder()
-
-	//Sets up the server to the endpoint.
-	server := httptest.NewServer(http.HandlerFunc(handlers.NotificationsHandler))
-
-	defer server.Close()
-	listOfIdsOfWebhooksToDelete := getIds()
-
-	for _, id := range listOfIdsOfWebhooksToDelete {
-		println(id)
-		url := server.URL + "/" + id
-		respondent := httptest.NewRequest(http.MethodDelete, url, nil)
-
-		handlers.DeleteWebhook(rec, respondent, "webhooks", id)
-
-		responseGetwebhook, err := client.Get(url)
-
-		if err != nil {
-			t.Errorf("Error sending get request to notification service " + err.Error())
-		}
-		var testHookDelete internal.Webhook
-
-		err = json.NewDecoder(responseGetwebhook.Body).Decode(&testHookDelete)
-		if err == nil {
-			t.Errorf("Error in deleting webhook, as we can retrieve it even after deletion" + err.Error())
-			t.Fatal()
-		}
-		handlers.GetWebhook(rec, "webhooks", responseGetwebhook.Request, testHookDelete.WebhookId)
-
-		// check test case results
-		webhook, errorWebhook := database.GetWebhook("webhooks", testHookDelete.WebhookId)
-		if errorWebhook == nil {
-			t.Fatal()
-		}
-
-		asrt := assert.New(t)
-
-		asrt.Equal(webhook, internal.Webhook{})
-		asrt.Equal(testHookDelete.WebhookId, "")
-
-	}
-
-	url := server.URL
-	respondent, err := client.Post(url, http.MethodDelete, nil)
-
-	handlers.DeleteWebhook(rec, respondent.Request, "webhooks", "")
-
-	asrt := assert.New(t)
-	asrt.Equal(http.StatusBadRequest, rec.Code)
-
-	allIdsDeleted()
-
-	asrt.Empty(SeveralIds, "Expect no more IDs in the list of registered webhooks within test file")
-}
-
 // TestGetWebhooks tests method of retrieving all the webhooks in the collection. Since this request goes to the actual
 // webhooks in firebase, we first register the number for how many there are now from GET request with unspecified id,
 // add a few, and then compare the number. For each webhook added, they are added to the list of webhooks created within
@@ -292,9 +221,6 @@ func TestGetWebhooks(t *testing.T) {
 		//Uses the request from registered response to make a new webhook registration. It is put into webhooks in firebase.
 		handlers.WebhookRegistration(rec, responseRegistration.Request, "webhooks")
 
-		newurl := server.URL + "/" + webhook.WebhookId
-		println(newurl)
-
 		//Here the id of the webhook is registered in the list of webhooks created within test environment, to be deleted
 		//after test.
 		registerTestingId(webhook.WebhookId)
@@ -313,4 +239,75 @@ func TestGetWebhooks(t *testing.T) {
 
 	asrt.Equal(http.StatusOK, record.Code)
 
+}
+
+// TestDeleteWebhook deletes the webhooks that has been added to the collection of webhook id's within this test file.
+// It ranges over this list, and deletes them by creating http DELETE requests, and passing this as parameter to
+// DeleteWebhook method of handler. The recorded http status code is then later compared, and a get request to the
+// individual webhooks is tried sent (and test is failed if it successfully manages to retrieve webhook supposed
+// to be deleted). Also empties list after all is deleted from list, and asserts it empty.
+func TestDeleteWebhook(t *testing.T) {
+
+	err := godotenv.Load()
+	if err != nil {
+		os.Exit(1)
+	}
+	database.FirebaseConnect()
+
+	//Initializes client.
+	client := http.Client{}
+
+	rec := httptest.NewRecorder()
+
+	//Sets up the server to the endpoint.
+	server := httptest.NewServer(http.HandlerFunc(handlers.NotificationsHandler))
+
+	defer server.Close()
+	listOfIdsOfWebhooksToDelete := getIds()
+
+	for _, id := range listOfIdsOfWebhooksToDelete {
+		println(id)
+		url := server.URL + "/" + id
+		respondent := httptest.NewRequest(http.MethodDelete, url, nil)
+
+		handlers.DeleteWebhook(rec, respondent, "webhooks", id)
+
+		responseGetwebhook, err := client.Get(url)
+
+		if err != nil {
+			t.Errorf("Error sending get request to notification service " + err.Error())
+		}
+		var testHookDelete internal.Webhook
+
+		err = json.NewDecoder(responseGetwebhook.Body).Decode(&testHookDelete)
+		if err == nil {
+			t.Errorf("Error in deleting webhook, as we can retrieve it even after deletion" + err.Error())
+			t.Fatal()
+		}
+		handlers.GetWebhook(rec, "webhooks", responseGetwebhook.Request, testHookDelete.WebhookId)
+
+		// check test case results
+		webhook, errorWebhook := database.GetWebhook("webhooks", testHookDelete.WebhookId)
+		if errorWebhook == nil {
+			t.Fatal()
+		}
+
+		asrt := assert.New(t)
+
+		asrt.Equal(webhook, internal.Webhook{})
+		asrt.Equal(testHookDelete.WebhookId, "")
+
+	}
+
+	url := server.URL
+	respondent, err := client.Post(url, http.MethodDelete, nil)
+
+	handlers.DeleteWebhook(rec, respondent.Request, "webhooks", "")
+
+	asrt := assert.New(t)
+	asrt.Equal(http.StatusBadRequest, rec.Code)
+
+	allIdsDeleted()
+
+	asrt.Empty(SeveralIds, "Expect no more IDs in the list of registered webhooks within test file")
 }
